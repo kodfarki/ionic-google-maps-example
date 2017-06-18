1a714bcfd2183c08fd19e87887bfda5ceaba1a54
Halil Karakose <halil.karakose@kodfarki.com>
2017-06-19 01:30:30 +0300

    added google maps code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { GoogleMap, GoogleMapsEvent, LatLng } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map: GoogleMap;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    platform.ready().then(() => {
+      this.loadMap();
+    });
   }
 
+  loadMap(){
+    let location = new LatLng(-34.9290,138.6010);
+
+    this.map = new GoogleMap('map', {
+      'backgroundColor': 'white',
+      'controls': {
+        'compass': true,
+        'myLocationButton': true,
+        'indoorPicker': true,
+        'zoom': true
+      },
+      'gestures': {
+        'scroll': true,
+        'tilt': true,
+        'rotate': true,
+        'zoom': true
+      },
+      'camera': {
+        'latLng': location,
+        'tilt': 30,
+        'zoom': 15,
+        'bearing': 50
+      }
+    });
+
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+      console.log('Map is ready!');
+    });
+
+  }
 }
